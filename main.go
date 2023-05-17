1836fbe032290b18c294e5bb4ac28140a04cfc38
Dyes Junior Sakwa <donjnr95@gmail.com>
2023-05-17 11:18:03 +0300

    Understanding variables

--- main.go
+++ main.go
@@ -4,6 +4,41 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hello guys")
+	//Starting out on variables
+	//strings
+
+	// var nameOne string = "one"
+	// var nameTwo = "Dyes"
+	// var nameThree string
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameOne = "Mercedes"
+	// nameThree = "Bowswe"
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameFour := "Mkuru"
+	// fmt.Println(nameFour)
+
+	//Integers
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	//bits and memory
+	// var numOne int8 = 28
+	// var numTwo int8= -128
+	// var numThree uint8 = 25
+
+	// Floats
+	// ?For floats you have to declare the bitsize
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 4747684868896766879.8
+	scoreThree := 1.6
+
+	fmt.Println(scoreOne, scoreTwo, scoreThree)
 
 }
